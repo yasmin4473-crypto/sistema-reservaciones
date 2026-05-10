aecc6d0afd3a7d0e5ec914407eaaf2780f939a0c
Claudio Guedez <claudioguedez12@gmail.com>
2026-05-10 13:38:50 -0400

    Fix host Railway

---
+++ _git_push.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d "C:\Users\Claudio Guedez\Desktop\sistema-reservaciones"
+del ".git\index.lock" 2>nul
+git config user.email "claudioguedez12@gmail.com"
+git config user.name "Claudio Guedez"
+git add app.py
+git commit -m "Set debug=False for production"
+git push
+echo.
+echo === Done! Press any key to close ===
+pause
