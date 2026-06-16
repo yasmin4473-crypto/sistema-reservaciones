c90e85b36efae321ec29ba66db154dc8ba65f6d3
Claudio Guedez <claudioguedez12@gmail.com>
2026-06-16 13:20:32 -0400

    Add privacy and terms pages for Twilio A2P compliance

--- _git_push.bat
+++ _git_push.bat
@@ -3,8 +3,8 @@ cd /d "C:\Users\Claudio Guedez\Desktop\sistema-reservaciones"
 del ".git\index.lock" 2>nul
 git config user.email "claudioguedez12@gmail.com"
 git config user.name "Claudio Guedez"
-git add app.py
-git commit -m "Set debug=False for production"
+git add app.py notificaciones.py landing.html
+git commit -m "Implement 3 Standard plan features: SMS reminders, monthly reports, PDF invoicing"
 git push
 echo.
 echo === Done! Press any key to close ===
