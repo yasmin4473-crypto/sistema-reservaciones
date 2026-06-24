0c863fb8524f7bfb8227e4869acb60f16dbfb0ab
Claudio Guedez <claudioguedez12@gmail.com>
2026-06-23 23:47:43 -0400

    Fix language detection for SMS/WhatsApp bot

--- _git_push.bat
+++ _git_push.bat
@@ -3,8 +3,6 @@ cd /d "C:\Users\Claudio Guedez\Desktop\sistema-reservaciones"
 del ".git\index.lock" 2>nul
 git config user.email "claudioguedez12@gmail.com"
 git config user.name "Claudio Guedez"
-git add app.py notificaciones.py landing.html
-git commit -m "Implement 3 Standard plan features: SMS reminders, monthly reports, PDF invoicing"
 git push
 echo.
 echo === Done! Press any key to close ===
